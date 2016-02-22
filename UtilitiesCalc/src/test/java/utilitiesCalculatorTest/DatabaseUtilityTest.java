aff057a5dd3d3e27f9d46d1e467b69c95d01a3c7
Kneuman0 <kylepneuman@gmail.com>
2016-02-22 08:35:35 -0500

    Created new jUnit. Tests to come

---
+++ UtilitiesCalc/src/test/java/utilitiesCalculatorTest/DatabaseUtilityTest.java
@@ -0,0 +1,14 @@
+package utilitiesCalculatorTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DatabaseUtilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
